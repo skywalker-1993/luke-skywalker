740ef8cc5611419f7fc8a8c28d24e614db816a68
skywalker-1993 <tiagorib93@gmail.com>
2023-01-13 17:50:28 +0100

    divided AWSSQS class into several chunks

---
+++ src/test/java/AWSManagement/AWSSQSClient.java
@@ -0,0 +1,63 @@
+package AWSManagement;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class AWSSQSClient {
+
+  //try {
+  //sqs.createQueue(create_request);
+  //    } catch (AmazonSQSException e) {
+  //      if (!e.getErrorCode().equals("QueueAlreadyExists")) {
+  //        throw e;
+  //      }
+  //    }
+  private static final String sqsEndpoint = "http://localhost:4566";
+  private static final String sqsRegion = "us-west-2";
+
+
+  public static AmazonSQS getSQSClient() {
+    return AmazonSQSAsyncClientBuilder.standard()
+        .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(sqsEndpoint, sqsRegion))
+        .build();
+  }
+
+  public static void createQueue(AmazonSQS client, String queueName) {
+    CreateQueueRequest createRequest = new CreateQueueRequest(queueName)
+        .addAttributesEntry("DelaySeconds", "60")
+        .addAttributesEntry("MessageRetentionPeriod", "86400");
+    client.createQueue(createRequest);
+  }
+
+  public static String getQueueUrl(AmazonSQS client, String queueName) {
+    for (String url: client.listQueues().getQueueUrls()) {
+      if (url.endsWith(queueName)) return url;
+    }
+    return "";
+  }
+
+  public static void sendMessage(AmazonSQS client, String queueUrl, String msg) {
+    SendMessageRequest sendMsgRequest = new SendMessageRequest()
+        .withQueueUrl(queueUrl)
+        .withMessageBody(msg)
+        .withDelaySeconds(5);
+    client.sendMessage(sendMsgRequest);
+  }
+
+  public static List<Message> receiveMessages(AmazonSQS client, String queueUrl) {
+    return client.receiveMessage(queueUrl).getMessages();
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ src/test/resources/MainTests/StreamProcessingTests.feature
@@ -0,0 +1,14 @@
+@parallel=true
+Feature: Demo Challenge Task #2 - Stream processing test
+
+  # TODO
+  Scenario: Successfully get users list - Validate Response Structure
+#    * call read('../TestBasis/ReqresBaseScenarios.feature@GetUsersDefault') {'expCode': 200}
+#    * configure continueOnStepFailure = { enabled: true, continueAfter: true, keywords: ['match', 'assert'] }
+#    * match response.page == 1
+#    * assert checkType("number", response.per_page)
+#    * assert checkType("number", response.total)
+#    * assert checkType("number", response.total_pages)
+#    * assert isObjectArray(response.data)
+#    * assert !isObjectArray(response.support)
+#    * configure continueOnStepFailure = { enabled: false, continueAfter: true, keywords: ['match', 'assert'] }
